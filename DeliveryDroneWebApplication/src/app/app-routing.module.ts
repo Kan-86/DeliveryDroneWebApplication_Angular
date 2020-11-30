0ee89bf1f4f0d5e7f9c287cad8112ca19bc07d1c
Sub-Z3r0 <andersen19@gmail.com>
2020-11-30 10:38:11 +0100

    routing for order page
    works

--- DeliveryDroneWebApplication/src/app/app-routing.module.ts
+++ DeliveryDroneWebApplication/src/app/app-routing.module.ts
@@ -12,7 +12,8 @@ const routes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
-  }];
+  },
+  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
